3481ac2748bb8f38fff4cfecfc1af542937f8547
Adam Berger <adam.g.berger@gmail.com>
2019-07-07 21:15:07 -0400

    Updated schema

--- src/calendar_defender/db.clj
+++ src/calendar_defender/db.clj
@@ -53,7 +53,61 @@
    {:db/ident :user.google/id
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/doc "User's google id"}])
+    :db/doc "User's google id"}
+   {:db/ident :n/id
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Node's id"}
+   {:db/ident :n/type
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "Node's type"}
+   {:db/ident :n/next
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/isComponent true
+    :db/doc "Node's nexts"}
+   {:db/ident :n/op
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "Operation"}
+   {:db/ident :n.op/args
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/isComponent true
+    :db/doc "Operation arguments"}
+   {:db/ident :n.op.args/type
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "Operation argument type"}
+   {:db/ident :n.op.args.str/str
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "String argument"}
+   {:db/ident :n.op.args.ref/ref
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Ref argument"}
+   {:db/ident :n.mult-choice/q
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Multiple choice node's question"}
+   {:db/ident :n.mult-choice/ans
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/many
+    :db/doc "Multiple choice node's answers"}
+   {:db/ident :n.decline/reason
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Decline node's decline reason"}
+   {:db/ident :n.type/mult-choice-single}
+   {:db/ident :n.type/decline}
+   {:db/ident :n.op/=}
+   {:db/ident :n.op/not}
+   {:db/ident :n.op/or}
+   {:db/ident :n.op/and}
+   {:db/ident :n.op.args.type/ref}
+   {:db/ident :n.op.args.type/str}])
 
 (defn ensure-schema []
   (d/transact (get-conn) {:tx-data schema}))
